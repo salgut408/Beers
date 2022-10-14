2bbb42920b226fe5af986d106a3db7e48f7f09ef
salgut408 <sgutie@saic.edu>
2022-10-13 22:24:10 -0500

    list working

--- app/src/main/java/com/sgut/android/beerapplication/listscreen/BeerCard.kt
+++ app/src/main/java/com/sgut/android/beerapplication/listscreen/BeerCard.kt
@@ -1,8 +1,5 @@
 package com.sgut.android.beerapplication.listscreen
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
@@ -12,27 +9,33 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Card
 import coil.compose.rememberAsyncImagePainter
 import coil.request.ImageRequest
 import com.sgut.android.beerapplication.data.domainmodels.BeerDomainModel
 
 @Composable
 fun BeerCard(beer: BeerDomainModel, modifier: Modifier) {
-    Surface(shape = RoundedCornerShape(10.dp), elevation = 9.dp, modifier = modifier) {
-        Column(modifier = Modifier.fillMaxWidth() ) {
+    Card(shape = RoundedCornerShape(10.dp), elevation = 9.dp, modifier = modifier,) {
+        Row(modifier = Modifier.fillMaxWidth() ) {
             //image
 
             val painter = rememberAsyncImagePainter(
                 model = ImageRequest.Builder(LocalContext.current)
                     .data(beer.imageUrl)
-                    .size(125)
+                    .size(60)
+
                     .build()
             )
            Image(painter = painter,contentDescription = beer.name, modifier = Modifier.size(200.dp))
-            Text(beer.name ?: "No Name", style = MaterialTheme.typography.h1)
+
+
+
             Column(modifier = Modifier.padding(16.dp)) {
+                Text(beer.name ?: "No Name", style = MaterialTheme.typography.h5)
                 Text(beer.tagline ?: "No Tagline")
+
             }
         }
     }
